72c12be2b84bad83eba402c7430f3e56537ab8bb
SADIO-M <megan_florence_sadio@dlsu.edu.ph>
2026-02-13 16:48:26 +0800

    Created 5 User Profiles in profiles.js, Added UI to ViewProfile

--- data/profiles.js
+++ data/profiles.js
@@ -4,6 +4,40 @@ let profiles = [
         bio: "Game Developer || 23 || Bill Cipher Defender",
         gameGenres: "None",
         employmentStatus: "FREELANCE",
+        profilePicture: "/img/PFP_1", 
+        bannerPicture: "/img/banner_1", 
+    },
+    {
+        username: "Si-Szas",
+        bio: "Project Manager | Quality Assurance | Game Developer | Gengar Enjoyer",
+        gameGenres: "Roguelikes, Action",
+        employmentStatus: "FREELANCE",
+        profilePicture: "/img/PFP_2", 
+        bannerPicture: "/img/banner_2", 
+    },
+    {
+        username: "Ayze",
+        bio: "MunDos Writing Lead / UI Programmer / White-hair, short-bangs women enjoyer",
+        gameGenres: "Casual :3",
+        employmentStatus: "FREELANCE",
+        profilePicture: "/img/PFP_3", 
+        bannerPicture: "/img/banner_3", 
+    },
+    {
+        username: "EV-Lot",
+        bio: "Just a silly 3D & VFX Artist girl",
+        gameGenres: "Horror, Fantasy, Action",
+        employmentStatus: "Currently Employed at Game Busters",
+        profilePicture: "/img/PFP_4", 
+        bannerPicture: "/img/banner_4", 
+    },
+    {
+        username: "TheRealValfyn",
+        bio: "Quality Assurance // Back-end Developer // Loves swimming",
+        gameGenres: "Fitness, Anything about the ocean",
+        employmentStatus: "UNEMPLOYED",
+        profilePicture: "/img/PFP_5", 
+        bannerPicture: "/img/banner_5", 
     }
 ];
 
