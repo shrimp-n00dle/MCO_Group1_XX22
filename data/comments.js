6f7bdf460fcab3028e991f609b03b1a02a3fc91a
solar-ym <yzabelle_montuerto@dlsu.edu.ph>
2026-02-13 21:47:28 +0800

    comments work

---
+++ data/comments.js
@@ -0,0 +1,11 @@
+let comments = [
+    {
+        postId: 1,
+        username: "Ayze",
+        profilePicture: "/img/PFP_3.png",
+        body: "Woah ur epic!!",
+        likeCount: "1",
+    },
+];
+
+module.exports = {comments};
