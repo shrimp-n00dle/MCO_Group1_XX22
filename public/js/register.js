15e4e1f20e031a7320d57d65fa41cd2300474ced
SADIO-M <megan_florence_sadio@dlsu.edu.ph>
2026-03-10 22:35:33 +0800

    Register fields required

--- public/js/register.js
+++ public/js/register.js
@@ -1,3 +1,13 @@
-function testRegister(){
-    document.getElementById('testReg').innerHTML = Date();
+function registerUser(){
+  var firstName = document.getElementById("firstName").required;
+  var lastName = document.getElementById("lastName").required;
+  var email = document.getElementById("email").required;
+  var username = document.getElementById("username").required;
+  var password = document.getElementById("password").required;
+  
+  document.getElementById("registerTest").innerHTML = firstName;
+  document.getElementById("registerTest").innerHTML = lastName;
+  document.getElementById("registerTest").innerHTML = email;
+  document.getElementById("registerTest").innerHTML = username;
+  document.getElementById("registerTest").innerHTML = password;
 }
