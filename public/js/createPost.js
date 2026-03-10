685e1b42c1aea755382877a73f77e2991d0a727d
shrimp-n00dle <vingno.elizabeth@gmail.com>
2026-03-10 22:03:15 +0800

    Posting [38%]
    -- added comment body

--- public/js/createPost.js
+++ public/js/createPost.js
@@ -61,10 +61,43 @@ function addPost()
     date_today.innerHTML = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDay() + "   " + date.getHours() + ":" + date.getMinutes();
     post_footer.appendChild(date_today);
 
+    /*TEMP ADDING OF COMMENT BODY********************************/
+    let reply_body = document.createElement("div");
+    reply_body.classList.toggle("commentBody");
+    reply_body.innerHTML = "Test";
+     /*LIKES RECEIVED*/
+    let reply_like = document.createElement("p");
+    reply_like.classList.toggle("postLikeCount");
+    reply_like.innerHTML = 0 + " Like Counts";
+    /*Like Button*/
+    let reply_like_button = document.createElement("button");
+    reply_like_button.classList.toggle("genButton-red");
+    reply_like_button.innerHTML = "Like This Post";
+    reply_like.appendChild(reply_like_button);    
+    reply_body.appendChild(reply_like);
+     /*COMMENTS RECEIVED**/
+    let reply_comm_count = document.createElement("p");
+    reply_comm_count.classList.toggle("postCommentCount");
+    reply_comm_count.innerHTML = 0 + " Comment Counts";
+    /*Comment Button*/
+    let reply_comm_button = document.createElement("button");
+    reply_comm_button.classList.toggle("genButton-red");
+    reply_comm_button.innerHTML = "Leave a Comment";
+    reply_comm_count.appendChild(reply_comm_button);    
+    reply_body.appendChild(reply_comm_count);
+    /******************************************************************** */
+
+
     /*BINDING*/
+    post_footer.appendChild(reply_body);
     post_body.appendChild(post_footer);
-    post_template.appendChild(post_body);
     main_page.appendChild(post_template);
+
+
+
+    
+
+
 
 
     //clear the text entrys
@@ -76,6 +109,14 @@ post_button.addEventListener("click",function(p)
 {
     addPost();
 }, false);
+
+
+
+
+function addComment()
+{
+
+}
 
 
 
